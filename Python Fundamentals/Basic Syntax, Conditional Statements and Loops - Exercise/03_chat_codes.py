952c84877d4ecd4b776899d1fbb175c7521f76ab
Ivan Vakov <vakov.ivan@gmail.com>
2024-02-08 18:18:49 +0200

    more solutions

---
+++ Python Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/03_chat_codes.py
@@ -0,0 +1,13 @@
+phone_number = int(input())
+
+for n in range(phone_number):
+    code = int(input())
+
+    if code == 88:
+        print("Hello")
+    elif code == 86:
+        print("How are you?")
+    elif code < 88:
+        print("GREAT!")
+    else:
+        print("Bye.")
