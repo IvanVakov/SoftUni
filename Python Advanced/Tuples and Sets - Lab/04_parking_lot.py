65d97911f0d14455ec71070784b82c2980663dfb
Ivan Vakov <vakov.ivan@gmail.com>
2024-09-23 12:38:09 +0300

    more solutions

---
+++ Python Advanced/Tuples and Sets - Lab/04_parking_lot.py
@@ -0,0 +1,15 @@
+number_of_commands = int(input())
+parking = set()
+
+for _ in range(number_of_commands):
+    direction, car_number = input().split(", ")
+
+    if direction == "IN":
+        parking.add(car_number)
+    elif direction == "OUT":
+        parking.remove(car_number)
+
+if parking:
+    print(*parking, sep="\n")
+else:
+    print("Parking Lot is Empty")
